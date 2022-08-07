8acf1bfff53caf0381f0139ca67728774d655043
Ld. Nelson <67654295+MwangiNelson@users.noreply.github.com>
2022-08-07 21:05:57 +0300

    section1 complete

--- app/Views/UI/landing.php
+++ app/Views/UI/landing.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Antic+Slab&family=Barlow+Condensed:wght@300&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Antic+Slab&family=Barlow+Condensed:wght@300&family=Lato:wght@300&display=swap" rel="stylesheet">
     <!-- tab icon -->
     <link rel="icon" href="assets/DukaKuu.png" type="image/x-icon">
+
+    <!-- fontawesome icons -->
+    <script src="https://kit.fontawesome.com/db540a34d6.js" crossorigin="anonymous"></script>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <!-- CSS only -->
@@ -31,7 +32,7 @@
             <img src="assets/DukaKuu.png" alt="" class="img-logo">
         </div>
         <div class="nav-options">
-            <ul class="lr jcc aic td-none lst-none c-white">
+            <ul class="lr jcc aic td-none lst-none c-white p-0 m-0">
                 <li class="px-3 c-white"><a href="">NEW ARRIVALS</a></li>
                 <li class="px-3 c-white"><a href="">MENSWEAR</a></li>
                 <li class="px-3 c-white"><a href="">LADIESWEAR</a></li>
@@ -48,6 +49,66 @@
         </div>
 
     </nav>
+    <section class="section-one ud w-100 p-4">
+        <div class="lr jsb aic w-100">
+            <div class="ud jsb w-50">
+                <div class="text w-50 px-4">
+                    <p class="f-lato italic">“Fashion is a form of self expression
+                        and an aoutonomy at a particular era. ” - Sir Gon Di'z</p>
+
+                    <div class="socials ud py-xthick">
+                        <h5 class="f-lato">FOLLOW OUR SOCIAL MEDIA</h5>
+                        <span class="line"></span>
+                        <p class="f-xlarge">
+                            <b>NEW ARRIVALS WILL BE POSTED ON OUR SOCIAL MEDIA</b>
+                        </p>
+                        <div class="social-links">
+                            <ul class="lr jss g-5 aic lst-none w-100">
+                                <li><i class="fa-brands fa-instagram"></i></li>
+                                <li><i class="fa-brands fa-facebook-square"></i></li>
+                                <li><i class="fa-brands fa-twitter-square"></i></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="lr aic g-5 w-50 py-thick jce">
+                <img src="assets/img1.jpg" alt="" class="img-display">
+                <img src="assets/img2.jpg" alt="" class="img-display">
+                <div class="ud h-fit">
+                    <button class="right-angle lr jcc aic">View more <i class="fa-solid fa-angle-right f-large px-3"></i> </button>
+                </div>
+            </div>
+        </div>
+        <div class="lr w-100 py-4">
+            <div class="ud jce aic g-5">
+                <p class="rotate"><b>SCROLL DOWN</b> </p>
+                <img src="assets/mouse-cursor.png" class="img-sm" alt="">
+            </div>
+            <div class="ud w-100 jcc aic g-5">
+                <div class="desc ud jcc aic w-100">
+                    <div class="top lr jcc py-3">
+                        <h3>GET YOUR</h3>
+                        <h3 class="px-2 c-white td-under">CUSTOM FABRICS</h3>
+                        <h3>AT</h3>
+                    </div>
+                    <div class="logo jcc aic">
+                        <img src="assets/DukaKuu.png" class="img-logo" alt="">
+                    </div>
+                    <div class="bn jcc aic py-3">
+                        <button class="view-store lr jcc aic ">
+                            VIEW STORE
+                            <span class="material-symbols-outlined f-xlarge px-3">
+                                north_east
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
 </body>
 
 </html>
