97630eb09c31843e29f35920f2f5da52b3bba41e
Angelo Kizito Makory <angelomakory@gmail.com>
2022-08-17 13:28:20 +0300

    Repaired admin backend for login

---
+++ app/Controllers/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+// use App\Models\AdminModel;
+
+class AdminController extends BaseController{
+    //Admin credenials are static(for now) hence stored in variables..
+    public function adminLogin(){
+        $adminEmail = 'admin@dukakuu.com';
+        $adminPassword = 'coderiters_123';
+
+        // $adminModel = new AdminModel();
+        if($this->request->getMethod==='post'){
+            if($this->request->getPost('email')===$adminEmail && $this->request->getPost('password') === $adminPassword){
+                return redirect()->to('home/admin');
+            }
+            else{
+                return redirect()->to('home/login');
+            }
+        }
+    }
+}
