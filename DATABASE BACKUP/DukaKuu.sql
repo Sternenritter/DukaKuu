f1d53def9c9bd1f8d005099ac78e0fdb5bc7a785
cypriankoech <cypriankibet46@gmail.com>
2022-08-18 14:54:44 +0300

    updated sql database

--- DATABASE BACKUP/DukaKuu.sql
+++ DATABASE BACKUP/DukaKuu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 16, 2022 at 10:37 AM
+-- Generation Time: Aug 18, 2022 at 01:53 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -20,6 +20,44 @@ SET time_zone = "+00:00";
 --
 -- Database: `DukaKuu`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `total` int(11) DEFAULT NULL,
+  `status` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_item`
+--
+
+CREATE TABLE `cart_item` (
+  `cart_item_id` int(11) NOT NULL,
+  `cart_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(20) NOT NULL,
+  `is_deleted` enum('0','1') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -46,11 +84,51 @@ CREATE TABLE `customers` (
 INSERT INTO `customers` (`user_id`, `username`, `first_name`, `last_name`, `email`, `password`, `is_deleted`, `created_at`, `updated_at`) VALUES
 (1, 'dummy_user', 'Dummy', 'User', 'dummyuser@gmail.com', 'DummyUser123', '0', '2022-08-16 09:18:06', '2022-08-16 09:18:06'),
 (4, 'cypriankoech', 'Cyprian', 'Koech', 'cypriankibet46@gmail.com', '$2y$10$nf/vrvYkYq0s9Z3J8L243.RGw6RIbwOz3uE/6Wm3h6sEieRRDUUc.', '0', '2022-08-16 09:37:12', '2022-08-16 09:37:12'),
-(5, 'qehecyhivo', 'Martha', 'Gonzales', 'rupuq@mailinator.com', '$2y$10$Px6m0rAQTwVWAb/UANrY2exCtwSpZ/WUKysLlixME.T3Rl0bHHSFe', '0', '2022-08-16 10:09:03', '2022-08-16 10:09:03');
+(5, 'qehecyhivo', 'Martha', 'Gonzales', 'rupuq@mailinator.com', '$2y$10$Px6m0rAQTwVWAb/UANrY2exCtwSpZ/WUKysLlixME.T3Rl0bHHSFe', '0', '2022-08-16 10:09:03', '2022-08-16 10:09:03'),
+(6, 'syxyr', 'Nelle', 'Joyce', 'hunyhubuto@mailinator.com', '$2y$10$.dSL1.H4HF9Rp74ZjiK2iuONy4fGcBquBR81Nsrhylh3MedosAqMG', '0', '2022-08-16 16:03:50', '2022-08-16 16:03:50'),
+(7, 'gysor', 'Patricia', 'Harvey', 'funocote@mailinator.com', '$2y$10$RzV/iJBxQwIZp07gd1AgDupZJWbYST599Su/2rUfA4r2FwUynq4bW', '0', '2022-08-16 18:58:02', '2022-08-16 18:58:02'),
+(8, 'tilidydalu', 'Jana', 'Finley', 'fixuzojaho@mailinator.com', '$2y$10$Y5IbK8KUiLEeM1nOU1wogOh/0BbgG2BDqj4cvvadJcJyy1CRKpy/i', '0', '2022-08-16 19:01:10', '2022-08-16 19:01:10'),
+(9, 'ryheluziwi', 'Penelope', 'Faulkner', 'lolovocu@mailinator.com', '$2y$10$OKbC3W2AZ0TvHeAb7zIH4OMrW2/RwrWFDIVy2o/dC.N1guKE2eFGC', '0', '2022-08-16 19:02:56', '2022-08-16 19:02:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `product_description` varchar(255) DEFAULT NULL,
+  `category` int(11) NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  `is_deleted` enum('0','1') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- Indexes for table `cart_item`
+--
+ALTER TABLE `cart_item`
+  ADD PRIMARY KEY (`cart_item_id`),
+  ADD KEY `fk_cart_id` (`cart_id`),
+  ADD KEY `fk_product_id` (`product_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `customers`
@@ -61,14 +139,68 @@ ALTER TABLE `customers`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `fk_category` (`category`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cart_item`
+--
+ALTER TABLE `cart_item`
+  MODIFY `cart_item_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `customers` (`user_id`);
+
+--
+-- Constraints for table `cart_item`
+--
+ALTER TABLE `cart_item`
+  ADD CONSTRAINT `fk_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`),
+  ADD CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `fk_category` FOREIGN KEY (`category`) REFERENCES `category` (`category_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
