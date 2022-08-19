8e1df41cf05b643d62864962a8e38b4ae23eebb6
Angelo Kizito Makory <angelomakory@gmail.com>
2022-08-19 13:20:59 +0300

    Function to add category  in category model done

--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -6,4 +6,13 @@ class CategoryModel extends Model {
     protected $table = 'category';
     protected $primary_key = 'category_id';
     protected $allowedFields = ['category_id', 'category_name', 'is_deleted'];
+
+    public function addCategory($categoryID, $categoryName, $isDeleted){
+        if($this->db->query("INSERT INTO category(category_id, catehory_name, is_deleted) VALUES('$categoryID', '$categoryName', '$isDeleted')")){
+            return "Success";
+        }
+        else{
+            return "Failed";
+        }
+    }
 }
