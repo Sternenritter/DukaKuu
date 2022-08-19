c556689eabdaab9ec7e97cbe0b943411a6dfd684
Angelo Kizito Makory <angelomakory@gmail.com>
2022-08-19 13:11:40 +0300

    Added functionality to add and delete products

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -6,4 +6,28 @@ class ProductModel extends Model {
     protected $table = 'product';
     protected $primary_key = 'product_id';
     protected $allowedFields = ['product_id', 'product_name', 'product_description', 'category_id', 'image_path', 'is_deleted'];
+
+    public function addProduct($productName, $productDescription, $categoryID, $imagePath, $isDeleted){
+
+        if($this->db->query("INSERT INTO product(product_name, product_description, category_id, image_path, is_deleted) VALUES ('$productName', '$productDescription', '$categoryID', '$imagePath', '$isDeleted)")){
+            return "Success";
+        }
+        else{
+            return "Failed";
+        }
+    }
+
+    public function deleteProduct($productID){
+
+        if($this->db->query = ("DELETE FROM product WHERE product_id= '$productID")){
+            return "Success";
+        }
+        else{
+            return "Failed";
+        }
+    }
+
+    public function editProduct(){
+        
+    }
 }
